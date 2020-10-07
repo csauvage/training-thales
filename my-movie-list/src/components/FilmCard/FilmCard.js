8d607698917e42d8a81c55386795ae0c9e671700
Clément SAUVAGE <hello@clementsauvage.me>
2020-10-07 09:41:22 +0200

    Add noCover

--- my-movie-list/src/components/FilmCard/FilmCard.js
+++ my-movie-list/src/components/FilmCard/FilmCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './FilmCard.sass';
+import noMoviesImage from  '../../no-movie.png'
 
 const FilmCard = ({film, favorite}) => {
 
     return (
         <div className={`FilmCard ${favorite ? 'FilmCard--Favorite' : ''}`.trimEnd()}>
             <image>
-                <img src={"https://image.tmdb.org/t/p/original" + film.poster_path}/>
+                <img src={!!film.poster_path ? "https://image.tmdb.org/t/p/original" + film.poster_path : noMoviesImage}/>
                 <h1>{film.title}</h1>
             </image>
         </div>
